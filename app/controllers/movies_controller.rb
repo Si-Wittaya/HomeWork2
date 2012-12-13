48654b376f74fa842c31df9efd073db61c62334d
Wittaya_SP <wittaya_sp@ubuntu.(none)>
2012-12-13 13:57:33 -0500

    home work part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,4 @@
 class MoviesController < ApplicationController
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +6,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort = params[:sort]
+    @movies = Movie.all(:order => @sort)
+    if(@sort == 'title')
+       @title = 'hilite'
+    end
+    if(@sort == 'release_date')
+       @release_date = 'hilite'
+    end
   end
 
   def new
